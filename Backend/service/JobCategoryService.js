c3e740a8e85b2a747f626eef44f68fe86f534494
Aviral2442 <aviralsrivastava24y15@gmail.com>
2025-10-30 14:31:57 +0530

    update job sub category data using sub category id api created

--- Backend/service/JobCategoryService.js
+++ Backend/service/JobCategoryService.js
@@ -266,3 +266,42 @@ exports.createJobSubCategory = async (data) => {
         throw error;
     }
 };
+
+
+// Update Job Sub Category Service
+exports.updateJobSubCategory = async (subcategoryId, data) => {
+
+    try {
+        const allowedFields = [
+            'subcategory_category_id',
+            'subcategory_name',
+            'subcategory_image',
+            'subcategory_slug',
+            'subcategory_status',
+            'subcategory_updated_at'
+        ];
+
+        const updateData = {};
+        for (const field of allowedFields) {
+            if (data[field] !== undefined) {
+                updateData[field] = data[field];
+            }
+        }
+
+        if (data.subcategory_name) {
+            updateData.subcategory_slug = data.subcategory_name.toLowerCase().replace(/\s+/g, '-');
+        }
+
+        const result = await JobSubCategoryModel.findByIdAndUpdate(subcategoryId, updateData, { new: true });
+        if (!result) {
+            return { status: 404, message: 'Subcategory not found' };
+        }
+
+        return {
+            result
+        };
+    } catch (error) {
+        console.error('Error in updateJobSubCategory Service:', error);
+        throw error;
+    }
+};
