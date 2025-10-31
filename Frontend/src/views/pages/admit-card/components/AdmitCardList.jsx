cf43639e40034dea86e8080fccb6164d03c123cf
anis5165 <anishkumargvm02@gmail.com>
2025-10-31 17:31:19 +0530

    Refactor admission-related components: update routing for AdmitCard and AddAdmitCard; add Results and AnswerKey components; remove unused AdmitCardList and related files.

---
+++ Frontend/src/views/pages/admit-card/components/AdmitCardList.jsx
@@ -0,0 +1,14 @@
+// const page =() =>{
+//     return <div>Admit Card Page</div>
+// }
+// export default page;
+
+import React from 'react'
+
+const AdmitCardList = () => {
+  return (
+    <div>AdmitCardList</div>
+  )
+}
+
+export default AdmitCardList
